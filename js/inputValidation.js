d11b02227899229f0cb3bda9c5f1c3f2e2ecdb9c
ingrot <ingrotman@mail.ru>
2022-03-21 15:23:52 +0300

    Input validations done. Готова валидация телефона, email, имен/фамилий/отчеств

--- js/inputValidation.js
+++ js/inputValidation.js
@@ -1,3 +1,8 @@
+// var wrongInputColor = "rgba(242, 12, 12, 0.14)";
+var wrongInputColor = "rgba(242, 12, 12, 0.31)";
+// var correctInputColor = "#c4f9cc70";
+var correctInputColor = "rgb(180, 251, 191)";
+
 var allInputs = document.getElementsByTagName("input");
 Array.from(allInputs).forEach(function(input){
     switch(input.type) {
@@ -8,22 +13,34 @@ Array.from(allInputs).forEach(function(input){
             input.addEventListener("input", validatePhone);
             break;
     }
+    if (input.classList.contains("inputValidateNames")) {
+        input.addEventListener("input", validateNames);
+    }
 });
 
 function validatePhone() {
     var val = this.value;
-    if (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(val)) {
-        this.style.backgroundColor = "#c4f9cc70";
+    if (/^[\+]*[7-8]{1}[9][0-9]{9}/.test(val)) {
+        this.style.backgroundColor = correctInputColor;
     } else {
-        this.style.backgroundColor = "rgba(242, 12, 12, 0.14)";
+        this.style.backgroundColor = wrongInputColor;
     }
 }
 
 function validateEmail() {
     var val = this.value;
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val)) {
-        this.style.backgroundColor = "#c4f9cc70";
+        this.style.backgroundColor = correctInputColor;
     } else {
-        this.style.backgroundColor = "rgba(242, 12, 12, 0.14)";
+        this.style.backgroundColor = wrongInputColor;
+    }
+}
+
+function validateNames() {
+    var val = this.value;
+    if (/^[1IА-Я][\-1IА-Яа-яЁё]*$/.test(val)) {
+        this.style.backgroundColor = correctInputColor;
+    } else {
+        this.style.backgroundColor = wrongInputColor;
     }
 }
