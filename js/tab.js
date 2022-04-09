6408afb631e4786d8010a22b2188124c17da95d8
Beslan <64842276+Beslan066@users.noreply.github.com>
2022-04-09 11:38:21 +0300

    Страница профиля и новые стили

---
+++ js/tab.js
@@ -0,0 +1,23 @@
+const tabsBtn = document.querySelectorAll(".tab__nav-btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item){
+    item.addEventListener("click", function(){
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+        if(! currentBtn.classList.contains('active')){
+            tabsBtn.forEach(function(item){
+                item.classList.remove('active');
+            });
+            tabsItems.forEach(function(item){
+                item.classList.remove('active');
+            });
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+        }
+    });
+}
+document.querySelector('.tab__nav-btn').click();
